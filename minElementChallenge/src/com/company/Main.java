77e2f055a1cff7e1716a1026874a9aea3a84e366
ayush <ayushdeveloper0711@gmail.com>
2020-09-13 15:48:55 +0530

    Java learning

---
+++ minElementChallenge/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter count :");
+        int count = s.nextInt();
+        int[] returnedArray=readIntegers(count);
+        System.out.println("Min :"+findMin(returnedArray));
+    }
+
+    private static int[] readIntegers(int count) {
+        int[] array = new int[count];
+        System.out.println("Enter " + count + " elements :\r");
+        for (int i = 0; i < count; i++) {
+            array[i] = s.nextInt();
+        }
+        return array;
+    }
+    private static int findMin(int[] array){
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<array.length;i++){
+            int value=array[i];
+            if(value<min){
+                min=value;
+            }
+        }
+        return min;
+    }
+}
