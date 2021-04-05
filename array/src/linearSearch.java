2d3a981f15e9109c9e8962e79989cefb4ccdc8a5
ayush <ayushdeveloper0711@gmail.com>
2021-04-05 15:00:49 +0530

    updated

--- array/src/linearSearch.java
+++ array/src/linearSearch.java
@@ -1,2 +1,20 @@
+import java.util.*;
 public class linearSearch {
+    public static void main(String[]args){
+        Scanner n=new Scanner(System.in);
+        int f=0;
+        int arr[]={1,5,25,85,65};
+        System.out.println("Enter the elements you want to search");
+        int item=n.nextInt();
+        for(int i=0;i<arr.length;i++){
+            if(item==arr[i]){
+                f=1;
+                System.out.println("Element found at index "+i);
+                break;
+            }
+        }
+        if(f==0){
+            System.out.printf("Element not found");
+        }
+    }
 }
