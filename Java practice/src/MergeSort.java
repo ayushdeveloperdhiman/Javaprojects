9116b91200967e5bc27f6908ad0cbccff8f37317
ayush <ayushdeveloper0711@gmail.com>
2021-07-30 10:07:10 +0530

    updated

--- Java practice/src/MergeSort.java
+++ Java practice/src/MergeSort.java
@@ -13,51 +13,9 @@ public class MergeSort {
         sort(array,0, array.length-1);
         System.out.println(Arrays.toString(array));
     }
-    static void merge(int[] arr, int beg, int mid, int end)
+    static void merge(int[] arr)
     {
-
-        int l = mid - beg + 1;
-        int r = end - mid;
-
-        int[] LeftArray= new int [l];
-        int[] RightArray = new int [r];
-
-        for (int i=0; i<l; i++)
-            LeftArray[i] = arr[beg + i];
-
-        for (int j=0; j<r; j++)
-            RightArray[j] = arr[mid + 1+ j];
-
-
-        int i = 0, j = 0;
-        int k = beg;
-        while (i<l&&j<r)
-        {
-            if (LeftArray[i] <= RightArray[j])
-            {
-                arr[k] = LeftArray[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = RightArray[j];
-                j++;
-            }
-            k++;
-        }
-        while (i<l)
-        {
-            arr[k] = LeftArray[i];
-            i++;
-            k++;
-        }
-
-        while (j<r)
-        {
-            arr[k] = RightArray[j];
-            j++;
-            k++;
-        }
+        
     }
 
     static void sort(int arr[], int beg, int end)
